86c147eaf7072de3ed16152884aece7705654d38
Esteban Patiño <esteban.pg45@gmail.com>
2023-09-04 15:12:30 -0500

    Using config

---
+++ config.js
@@ -0,0 +1,17 @@
+DB_USER = "software2_practica1"
+DB_PASSWORD = "software2_practica1"
+DB_HOST = "cluster1.n2tcl.mongodb.net"
+
+// Configuracion de la IP del equipo
+IP_SERVER = "localhost"
+API_VERSION = "v1"
+JWT_SECRET_KEY = "clave_ecreta_curso"
+
+module.exports = {
+    DB_HOST,
+    DB_PASSWORD,
+    DB_USER,
+    IP_SERVER,
+    API_VERSION,
+    JWT_SECRET_KEY
+}
